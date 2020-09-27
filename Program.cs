1580895ada09f4d2ec6a5b4084575c0c556c65f2
jgrissom <jgrissom91@gmail.com>
2020-09-26 21:44:44 -0500

    Display selected category

--- Program.cs
+++ Program.cs
@@ -93,6 +93,8 @@ namespace NorthwindConsole
                         int id = int.Parse(Console.ReadLine());
                         Console.Clear();
                         logger.Info($"CategoryId {id} selected");
+                        Categories category = db.Categories.FirstOrDefault(c => c.CategoryId == id);
+                        Console.WriteLine($"{category.CategoryName} - {category.Description}");
                     }
                     Console.WriteLine();
 
